a6a5c7dec2052be661ff9e0da73fd23b208ec10e
Luna Shuqair <luna.shuqair89@gmail.com>
2021-02-06 13:40:21 -0500

    update

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ app.use(express.json());
 app.use(express.static('./develop/public'));
 
 // app.use(express.static(__dirname + "/Develop/public"));
-require("./Develop/routes/apiRoutes")(app);
-require("./Develop/routes/htmlRoutes")(app);
+require("./develop/routes/apiRoutes")(app);
+require("./develop/routes/htmlRoutes")(app);
 
-const dbJson = require("./Develop/db/db.json");
+const dbJson = require("./develop/db/db.json");
 
 // LISTENER
 // The below code effectively "starts" our server
